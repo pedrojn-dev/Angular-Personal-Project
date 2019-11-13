0e9859282e973b9c735fbab8c8e6a4d8d4a12fde
Pedro Jaramillo <pjn.fcc@gmail.com>
2019-11-12 19:04:38 -0500

    Config Search

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,6 +9,7 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor( private http: HttpClient ) {
 
@@ -18,13 +19,62 @@ export class ProductosService {
 
   private cargarProductos() {
 
-    this.http.get('https://angular-html-66529.firebaseio.com/productos_idx.json')
-    .subscribe( (resp: Producto[]) => {
+    return new Promise (  ( resolve, reject ) => {
 
-      console.log(resp);
-      this.productos = resp;
-      this.cargando = false;
+      this.http.get('https://angular-html-66529.firebaseio.com/productos_idx.json')
+          .subscribe( (resp: Producto[]) => {
+            this.productos = resp;
+            this.cargando = false;
+            resolve();
+          });
 
+    });
+
+  }
+
+  getProducto( id: string ) {
+
+    return this.http.get(`https://angular-html-66529.firebaseio.com/productos/${ id }.json`)
+
+
+  }
+
+  buscarProducto( termino: string ) {
+
+    if ( this.productos.length === 0 ) {
+      //cargar productos
+      this.cargarProductos().then( () => {
+        // ejecutar después de tener los productos
+        // Aplicar filtro
+        this.filtrarProductos( termino );
+      });
+    } else {
+      //aplicar el filtro
+      this.filtrarProductos( termino );
+    }
+
+    this.productosFiltrado = this.productos.filter( producto => {
+      return true;
+    });
+
+    console.log( this.productosFiltrado );
+
+  }
+
+  private filtrarProductos( termino: string ) {
+
+    console.log(this.productos);
+    this.productosFiltrado = [];
+
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach( prod => {
+
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+
+      if ( prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0 ) {
+          this.productosFiltrado.push( prod );
+      }
 
     });
 
